b9ea2c996c1619bc9795945f9930b9063b16c350
ncaq <ncaq@ncaq.net>
2025-12-20 12:04:31 +0900

    ci: Nixのsubstitutersとtrusted public keysを追加
    cachixの新しい個人リポジトリを追加。

--- flake.nix
+++ flake.nix
@@ -60,4 +60,17 @@
       );
       homeManagerModules.default = import ./modules/home-manager.nix { inherit self; };
     };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://git-hooks.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "git-hooks.cachix.org-1:t3VIYDdXlezkNY1/sRtYKzxMVKTgn+uAR9VWCXHRPeI="
+    ];
+  };
 }
