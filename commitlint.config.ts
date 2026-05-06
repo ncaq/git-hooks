92f084894cf9a60bb8ce2d988fa9d11fe8de91e3
ncaq <ncaq@ncaq.net>
2026-05-06 12:51:12 +0900

    refactor: 既存のルールの並び換えとコメントの調整

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,17 +3,17 @@ import { RuleConfigSeverity } from "@commitlint/types";
 import { plugin as userPlugin, type RulesConfig as UserRulesConfig } from "./src/@commitlint/rules/index";
 
 const rules: Partial<RulesConfig & UserRulesConfig> = {
-  // 日本語なども含めた可読文字で終わることを求める。
-  "subject-alnum-stop": [RuleConfigSeverity.Error, "never"],
-
-  // 件名の長さはGit公式推奨の50文字は厳しすぎるのである程度緩和します。
+  // ヘッダの幅は72文字まで。Git公式推奨の50文字は厳しすぎるので緩和。
   "header-max-length": [RuleConfigSeverity.Error, "always", 72],
-
-  // 本文の1行幅はGit公式推奨の72文字は現代のターミナルを考えると厳しすぎるので100文字まで。
+  // ボディの1行幅は100文字まで。Git公式推奨の72文字は現代のターミナルの幅では合理的ではないです。
   "body-max-line-length": [RuleConfigSeverity.Error, "always", 100],
+  // フッタの1行幅は100文字まで。Git公式推奨の72文字は現代のターミナルの幅では合理的ではないです。
   "footer-max-line-length": [RuleConfigSeverity.Error, "always", 100],
 
-  // 関数などの識別子などを直接コミットメッセージのタイトルに書きたいので無効にする。
+  // タイトルは日本語を含めたUnicode可読文字で終わることを求めます。
+  "subject-alnum-stop": [RuleConfigSeverity.Error, "never"],
+
+  // 関数などの識別子などを直接コミットメッセージのタイトルに書きたいのでcase識別は無効にします。
   "subject-case": [RuleConfigSeverity.Disabled],
 };
 
